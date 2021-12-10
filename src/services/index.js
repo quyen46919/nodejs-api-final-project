93217ab3f7a7ba08c810ba0dc191747d549b746c
taihoang <tai47406@donga.edu.vn>
2021-12-10 22:14:16 +0700

    clear file index

--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,2 @@
-// module.exports.authService = require('./auth.service');
-// // module.exports.emailService = require('./email.service');
-// module.exports.tokenService = require('./token.service');
-// module.exports.userService = require('./user.service');
-
-// module.exports.sinhVienService = require('./sinh-vien.service');
 module.exports.cardService = require('./card.service');
 module.exports.noteService = require('./note.service');
-// module.exports.khoaService = require('./khoa.service');
-// module.exports.monHocService = require('./mon-hoc.service');
-// module.exports.ketQuaService = require('./ket-qua.service');
-
-// module.exports.baiTap1Service = require('./bai-tap1.service');
