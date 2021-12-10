40fd78ea6d63500062414a0300e9cb4bdd74893b
Quochuy2809 <huyhuydai059@gmail.com>
2021-12-10 12:15:48 +0700

    CRUD for board model

---
+++ src/validations/board.validation.js
@@ -0,0 +1,63 @@
+const Joi = require('joi');
+const { password, objectId } = require('./custom.validation');
+const { Board } = require('../models');
+
+const createBoard = {
+  body: Joi.object().keys({
+    owner:  Joi.string().custom(objectId),
+    title: Joi.string().required(),
+    columnOrder: Joi.array(),
+    column: Joi.array(),
+    lastUpdated: Joi.date(),
+    isFavorite: Joi.boolean(),
+    sharedUserList: Joi.array(),
+  }),
+};
+
+const getBoards = {
+  query: Joi.object().keys({
+    title: Joi.string(),
+    role: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
+};
+
+const getBoard = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(objectId),
+  }),
+};
+
+
+const updateBoard = (
+  {
+  params: Joi.object().keys({
+    id: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+        title: Joi.string(),
+        columnOrder: Joi.array().items(Joi.custom(objectId)),
+        column: Joi.array().items(Joi.object().keys().min(1)),
+        lastUpdated: Joi.date(),
+        isFavorite: Joi.boolean(),
+        sharedUserList: Joi.array().items(Joi.custom(objectId)),
+    })
+    .min(1),
+});
+
+const deleteBoard = {
+  params: Joi.object().keys({
+    id: Joi.string().custom(objectId),
+  }),
+};
+
+module.exports = {
+    createBoard,
+    getBoards,
+    getBoard,
+    updateBoard,
+    deleteBoard,
+};
