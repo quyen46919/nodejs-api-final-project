16e79e1220a457ef3969bf35b8d080f810e0bf89
thanhtu <vithanhtu21>
2021-12-14 11:46:54 +0700

    fix eslin column

--- src/routes/v1/column.route.js
+++ src/routes/v1/column.route.js
@@ -5,11 +5,9 @@ const columnController = require('../../controllers/column.controller');
 
 const router = express.Router();
 
-
-router.post('/', validate(columnValidation.createColumn), columnController.createColumn)
-router.patch('/:id', validate(columnValidation.updateColumn), columnController.updateColumn)
-router.delete('/:id', validate(columnValidation.deleteColumn), columnController.deleteColumn)
-router.get('/', validate(columnValidation.queryColumns), columnController.getColumns)
-
+router.post('/', validate(columnValidation.createColumn), columnController.createColumn);
+router.patch('/:id', validate(columnValidation.updateColumn), columnController.updateColumn);
+router.delete('/:id', validate(columnValidation.deleteColumn), columnController.deleteColumn);
+router.get('/', validate(columnValidation.queryColumns), columnController.getColumns);
 
 module.exports = router;
